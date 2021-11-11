17dc5dd1a9c94b4b7a1ef71e3b78ec1938216794
AlecDubouzet <85209846+wrobe71279@users.noreply.github.com>
2021-11-11 14:21:04 +1100

    Main Menu with Working Buttons
    Main Menu with Working Buttons

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    //PlayButton is pressed
+    public void PlayGame()
+    {
+        Debug.Log("Playing Game...");
+        //SceneManager.LoadScene(sceneName: "Main");
+    }
+
+    /*public void StartTutorial()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("Start Tutorial...");
+    }
+    
+    public void SkipTutorial()
+    {
+        SceneManager.LoadScene(sceneName: "GameScene");
+        Debug.Log("Skip Tutorial...");
+    }
+    */
+
+    public void Quit()
+    {
+        Debug.Log("Quitting Scene...");
+        Application.Quit();
+    }
+
+    //for planned Scene Transition Animations
+    /*
+    IEnumerator LoadScene()
+    {
+        transitionAnim.SetTrigger("end");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(sceneName);
+    }
+    */
+
+}
