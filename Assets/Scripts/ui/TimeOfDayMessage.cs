07b0089f4f36b501d6579c82f4032acf6de6d575
AlecDubouzet <85209846+wrobe71279@users.noreply.github.com>
2021-11-15 14:12:53 +1100

    Revert "Time of Day UI displays day count and warns player of upcoming Night"
    This reverts commit 5adf81a8b8ec7a38d5126d1ab9d569b854ab8d49.

--- Assets/Scripts/ui/TimeOfDayMessage.cs
+++ Assets/Scripts/ui/TimeOfDayMessage.cs
@@ -8,53 +8,28 @@ public class TimeOfDayMessage : MonoBehaviour
 
     public GameObject timeOfDayPanel;
     public Text messageText;
-    public LightingManager lightingMngr;
-
-    public bool active = true;
-    public bool nightTime = false;
-
-    public int currentDayCount;
     
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(MessagePopup());
-        currentDayCount = lightingMngr.daycount;
+        messageText.text = ("Day 1");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(currentDayCount<lightingMngr.daycount && nightTime == true)
-        {
-            StartCoroutine(MessagePopup());
-        }
-
-        if(lightingMngr.timeOfDay >= 17 && active == true)
-        {
-            StartCoroutine(NightTimePopup());
-        }
+        
     }
 
     void PlayMessage()
     {
         timeOfDayPanel.SetActive(true);
-        nightTime = false;
-        active = true;
-        messageText.text = ("Day " + lightingMngr.daycount);
     }
 
     void CloseMessage()
     {
         timeOfDayPanel.SetActive(false);
-    }
-
-    void NightMessage()
-    {
-        timeOfDayPanel.SetActive(true);
-        active = false;
-        nightTime = true;
-        messageText.text = ("Night is approaching... Get Ready!");
     }
 
     IEnumerator MessagePopup()
@@ -63,10 +38,5 @@ public class TimeOfDayMessage : MonoBehaviour
         yield return new WaitForSeconds(5f);
         CloseMessage();
     }
-    IEnumerator NightTimePopup()
-    {
-        NightMessage();
-        yield return new WaitForSeconds(5f);
-        CloseMessage();
-    }
+
 }
